04d65e719f05305d505d8350470ce130cdee7fbb
Umer <an01985i@gmail.com>
2020-11-08 22:58:22 -0600

    initial commit

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
@@ -1,5 +1,33 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
 public class Skill extends AbstractEntity {
+    @ManyToMany(mappedBy = "skills")
+    private List<Job> jobs=new ArrayList<>();
+
+    @NotNull
+    @Size(max = 50,message = "Description too long:")
+    private String description;
+    public Skill (){ }
+
+    public Skill(String newDescription){
+        super();
+        this.description = newDescription;
+    }
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 
 }
